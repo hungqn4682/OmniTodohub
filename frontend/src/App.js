e3a5af2f8000677146a52f359c04fafdfacdc1fb
hungqn4682 <fcluongnong1@gmail.com>
2022-03-03 21:09:54 +0700

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
-
+import Footer from './components/Footer'
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-     hello world
+      <Header/>
+      <Footer/>
     </div>
   );
 }
